6f5c6ca3a7447094b26d05c356ef5bc4c104ff07
gaomingjun <wisarmy@gmail.com>
2022-05-26 15:27:15 +0800

    add template

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,23 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
 
+use tauri::{AboutMetadata, Menu, MenuEntry, MenuItem, Submenu};
+
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    let ctx = tauri::generate_context!();
+    let menu = Menu::with_items([Submenu::new(
+        &ctx.package_info().name,
+        Menu::with_items([MenuItem::About(
+            ctx.package_info().name.clone(),
+            AboutMetadata::default(),
+        )
+        .into()]),
+    )
+    .into()]);
+    tauri::Builder::default()
+        .menu(menu)
+        .run(ctx)
+        .expect("error while running tauri application");
 }
--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,83 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <a-layout has-sider style="height:100%">
+        <a-layout-sider :style="{overflow: 'auto', height: '100vh', position: 'fixed', left: 0, top: 0, bottom: 0 }">
+          <a-menu
+            v-model:selectedKeys="selectedKeys2"
+            v-model:openKeys="openKeys"
+            mode="inline"
+            style="height: 100%"
+          >
+            <a-sub-menu key="sub1">
+              <template #title>
+                <span>
+                  <user-outlined />
+                  subnav 1
+                </span>
+              </template>
+              <a-menu-item key="1">option1</a-menu-item>
+              <a-menu-item key="2">option2</a-menu-item>
+              <a-menu-item key="3">option3</a-menu-item>
+              <a-menu-item key="4">option4</a-menu-item>
+            </a-sub-menu>
+            <a-sub-menu key="sub2">
+              <template #title>
+                <span>
+                  <laptop-outlined />
+                  subnav 2
+                </span>
+              </template>
+              <a-menu-item key="5">option5</a-menu-item>
+              <a-menu-item key="6">option6</a-menu-item>
+              <a-menu-item key="7">option7</a-menu-item>
+              <a-menu-item key="8">option8</a-menu-item>
+            </a-sub-menu>
+            <a-sub-menu key="sub3">
+              <template #title>
+                <span>
+                  <notification-outlined />
+                  subnav 3
+                </span>
+              </template>
+              <a-menu-item key="9">option9</a-menu-item>
+              <a-menu-item key="10">option10</a-menu-item>
+              <a-menu-item key="11">option11</a-menu-item>
+              <a-menu-item key="12">option12</a-menu-item>
+            </a-sub-menu>
+          </a-menu>
+        </a-layout-sider>
+
+    <a-layout style="padding-left:200px">
+    <a-layout-content style="height:100%;">
+          Content
+    </a-layout-content>
+    <a-layout-footer style="text-align: center">
+      Ant Design ©2018 Created by Ant UED
+    </a-layout-footer>
+    </a-layout>
+  </a-layout>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
+import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons-vue';
+import { defineComponent, ref } from 'vue';
+export default defineComponent({
   components: {
-    HelloWorld
-  }
-}
-</script>
+    UserOutlined,
+    LaptopOutlined,
+    NotificationOutlined,
+  },
 
+  setup() {
+    return {
+      selectedKeys1: ref(['2']),
+      selectedKeys2: ref(['1']),
+      openKeys: ref(['sub1']),
+    };
+  },
+
+});
+</script>
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
 }
 </style>
